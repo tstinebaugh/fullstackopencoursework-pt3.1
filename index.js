7ba0c8a37f445d2048261db5000bc1f7cf6f3e4c
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2022-10-26 16:14:40 -0700

    Add build

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static('build'))
+
 morgan.token('params', function(req, res) {
   return JSON.stringify(req.body)
 })
