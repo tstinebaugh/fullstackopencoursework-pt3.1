aeafb370d89240f678df167e2972c3244c5f0769
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2022-12-20 15:16:55 -0800

    3.14

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(cors())
@@ -14,68 +17,41 @@ morgan.token('params', function(req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms  :params'))
 
-const generateId = () => {
-  const maxId = people.length > 0
-    ? Math.max(...people.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-let people = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/info', (request, response) => {
-  response.send(`
-  <p>Phonebook has info for ${people.length} people</p>
-  <p>Request made ${new Date().toString()}</p>
-  `)
+  Person.find({}).then(people => {
+    response.send(`
+      <p>Phonebook has info for ${people.length} people</p>
+      <p>Request made ${new Date().toString()}</p>
+    `)
+  })
 })
 
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(people => {
     response.json(people)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = people.find(note => note.id === id)
-
-  if (person) {
+  Person.findById(request.params.id).then(person => {
+    if (person) {
       response.json(person)
-  } else {
-      response.status(404).end()
-  }
+    } else {
+        response.status(404).end()
+    }  
+  })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  people = people.filter(p => p.id !== id)
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   people = people.filter(p => p.id !== id)
 
-  response.status(204).end()
-})
+//   response.status(204).end()
+// })
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -86,26 +62,18 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  const filteredPeople = people.filter(person => person.name.toLowerCase() === body.name.toLowerCase())
-  if (filteredPeople.length > 0) {
-    return response.status(400).json({ 
-      error: 'name already added to phonebook' 
-    })
-  }
-  
-  const person = {
-      name: body.name,
-      number: body.number,
-      date: new Date(),
-      id: generateId(),
-  }
-  
-  people = people.concat(person)
-  
-  response.json(person)
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+    date: new Date(),
   })
+  
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
