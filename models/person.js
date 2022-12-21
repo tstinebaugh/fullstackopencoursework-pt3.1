19e0831e2e0c749082c6805dfa1ecece68ca8d36
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2022-12-20 16:09:29 -0800

    3.19

--- models/person.js
+++ models/person.js
@@ -13,8 +13,15 @@ mongoose.connect(url)
   })
 
 const peopleSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true
+  },
   date: Date,
 })
 
